380982f6e6627cb2efd91a18c802cc94f977578e
Elias Feijó <eliasfeijo.dev@gmail.com>
2023-11-23 06:03:49 -0300

    refactor: radio group for choosing between :root or custom selector

--- popup/popup.js
+++ popup/popup.js
@@ -1,93 +1,97 @@
+const DEFAULT_SELECTOR = 'body';
+const DEFAULT_CLASS = '__scrollbar-preferences-ext';
 const DEFAULT_SCROLLBAR_WIDTH_PX = 15;
-const SCROLLBAR_USE_RADIO = { CUSTOM: 'custom', DEFAULT: 'default' };
+const DEFAULT_PREFERENCES_OBJ = {
+  width: DEFAULT_SCROLLBAR_WIDTH_PX,
+  selector: '',
+};
+const USE_SELECTOR_RADIO = { CUSTOM: 'custom', ROOT: 'root' };
 
 function setScrollbarWidthVar(widthVar) {
   document.body.style.setProperty('--scrollbar-width', widthVar);
 }
 
-async function getScrollbarWidth() {
+async function getScrollbarPreferences() {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   if (!tabs || !tabs.length) return;
   const currentTab = tabs[0];
   const storage = await chrome.storage.local.get(currentTab.url);
-  const width = storage[currentTab.url];
-  const widthPx = width !== undefined ? `${width}px` : 'auto';
-  await chrome.scripting.executeScript({
-    target: { tabId: currentTab.id },
-    func: setScrollbarWidthVar,
-    args: [widthPx],
-  });
-  document.querySelector('#scrollbar-width-input').value = width ?? "";
-  console.log('getScrollbarWidth', width, currentTab)
-  return width;
+  const obj = {...DEFAULT_PREFERENCES_OBJ, ...storage[currentTab.url]};
+  // const width = storage[currentTab.url];
+  // const widthPx = width !== undefined ? `${width}px` : 'auto';
+  // await chrome.scripting.executeScript({
+  //   target: { tabId: currentTab.id },
+  //   func: setScrollbarWidthVar,
+  //   args: [widthPx],
+  // });
+  const isCustom = !!obj.selector && obj.selector !== ':root';
+  document.querySelector('#scrollbar-width-input').value = obj.width;
+  document.querySelector('#use-root-selector').checked = !isCustom;
+  document.querySelector('#use-custom-selector').checked = isCustom;
+  const selectorInput = document.querySelector('#custom-selector-input')
+  selectorInput.value = obj.selector || ':root';
+  selectorInput.disabled = !isCustom;
+  const selectorLabel = selectorInput.parentNode.querySelector('label');
+  selectorLabel.classList.toggle('disabled', !isCustom);
+  console.log('getScrollbarPreferences', obj, currentTab)
+  return obj;
 }
 
-getScrollbarWidth();
+getScrollbarPreferences();
 
 document.querySelector('#scrollbar-width-input').addEventListener('change', async (event) => {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   if (!tabs || !tabs.length) return;
   const currentTab = tabs[0];
   const width = parseInt(event.target.value, 10);
-  await chrome.storage.local.set({ [currentTab.url]: width });
-  await getScrollbarWidth();
+  const storage = await chrome.storage.local.get(currentTab.url);
+  const obj = {...DEFAULT_PREFERENCES_OBJ, ...storage[currentTab.url], width};
+  await chrome.storage.local.set({ [currentTab.url]: obj });
+  await getScrollbarPreferences();
   console.log('scrollbar-width-input changed', width, currentTab);
 });
 
 function addOrRemoveScrollbarPreferencesClass(shouldAddClass) {
   const func = shouldAddClass ? document.body.classList.add : document.body.classList.remove;
-  func.call(document.body.classList, '__scrollbar-preferences-ext');
+  func.call(document.body.classList, DEFAULT_CLASS);
 }
 
-async function onScrollbarUseChange(radioValue) {
-  console.log('onScrollbarUseChange', radioValue);
+async function onUseSelectorChange(radioValue) {
+  console.log('onUseSelectorChange', radioValue);
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   if (!tabs || !tabs.length) return;
   const currentTab = tabs[0];
-  const width = await getScrollbarWidth();
-  const input = document.querySelector('#scrollbar-width-input');
-  const isCustom = radioValue === SCROLLBAR_USE_RADIO.CUSTOM;
-  if (isCustom) {
-    input.disabled = false;
-    const widthOverride = width ?? (input.value || DEFAULT_SCROLLBAR_WIDTH_PX);
-    await chrome.storage.local.set({ [currentTab.url]: widthOverride });
-  } else {
-    input.disabled = true;
-    await chrome.storage.local.remove(currentTab.url);
-  }
-  await chrome.scripting.executeScript({
-    target: { tabId: currentTab.id },
-    func: addOrRemoveScrollbarPreferencesClass,
-    args: [isCustom],
-  });
-  await getScrollbarWidth();
+  const storage = await chrome.storage.local.get(currentTab.url);
+  const obj = {...DEFAULT_PREFERENCES_OBJ, ...storage[currentTab.url]};
+  obj.selector = radioValue === USE_SELECTOR_RADIO.ROOT ? '' : obj.selector || DEFAULT_SELECTOR;
+  await chrome.storage.local.set({ [currentTab.url]: obj });
+  await getScrollbarPreferences();
+  // await chrome.scripting.executeScript({
+  //   target: { tabId: currentTab.id },
+  //   func: addOrRemoveScrollbarPreferencesClass,
+  //   args: [isCustom],
+  // });
+  // await getScrollbarWidth();
 }
 
-document.querySelectorAll('input[name="scrollbar-use-radio"]').forEach((radio) => {
+document.querySelectorAll('input[name="selector-radio"]').forEach((radio) => {
   radio.addEventListener('click', () => {
-    onScrollbarUseChange(radio.value);
+    onUseSelectorChange(radio.value);
   });
 });
 
-/*
- * On popup open, initialize radio buttons to either "default" or "custom"
- * based on the scrollbar width for the current tab URL.
- * 
- * - If scrollbar width is not set, use "default" and disable the scrollbar width input.
- * - If scrollbar width is set, use "custom" and enable the scrollbar width input.
- */
-(async () => {
-  console.log('popup opened');
-  const width = await getScrollbarWidth();
-  const [checked, unchecked] = width !== undefined ? [SCROLLBAR_USE_RADIO.CUSTOM, SCROLLBAR_USE_RADIO.DEFAULT] : [SCROLLBAR_USE_RADIO.DEFAULT, SCROLLBAR_USE_RADIO.CUSTOM];
-  document.querySelector(`#scrollbar-use-${checked}`).checked = true;
-  document.querySelector(`#scrollbar-use-${unchecked}`).checked = false;
-  onScrollbarUseChange(checked);
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-  if (!tabs || !tabs.length) return;
-  const currentTab = tabs[0];
-  await chrome.scripting.insertCSS({
-    css: `body.__scrollbar-preferences-ext::-webkit-scrollbar { width: var(--scrollbar-width); }`,
-    target: { tabId: currentTab.id },
-  });
-})();
+// (async () => {
+//   console.log('popup opened');
+//   const preferences = await getScrollbarPreferences();
+//   const isCustom = !!preferences.selector && preferences.selector !== ':root';
+//   document.querySelector('#use-custom-selector').checked = isCustom;
+//   document.querySelector('#use-root-selector').checked = !isCustom;
+//   onUseSelectorChange(isCustom ? USE_SELECTOR_RADIO.CUSTOM : USE_SELECTOR_RADIO.ROOT);
+  // const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  // if (!tabs || !tabs.length) return;
+  // const currentTab = tabs[0];
+  // await chrome.scripting.insertCSS({
+  //   css: `${DEFAULT_SELECTOR}.${DEFAULT_CLASS}::-webkit-scrollbar { width: var(--scrollbar-width) !important; }`,
+  //   target: { tabId: currentTab.id },
+  // });
+// })();
